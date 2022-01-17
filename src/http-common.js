34911b382f7b9ee67c485214a349f4bf2443846b
yuayixio <felix.xiong@web.de>
2022-01-17 18:34:51 +0100

    Fix baseURL

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://express-akw.herokuapp.com/api",
+  baseURL: "https://express-akw.herokuapp.com:8080/api",
   headers: {
     "Content-type": "application/json"
   }
