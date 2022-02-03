234ef0f37a143cb7808482a9ac9d503b0adb9d82
yuayixio <felix.xiong@web.de>
2022-02-03 17:29:55 +0100

    Swap http-common back

--- src/http-common.js
+++ src/http-common.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
- // baseURL: "https://express-akw.herokuapp.com/api",
- baseURL: "http://localhost:8080/api", 
+ baseURL: "https://express-akw.herokuapp.com/api",
+ //baseURL: "http://localhost:8080/api", 
  headers: {
     "Content-type": "application/json"
   }
